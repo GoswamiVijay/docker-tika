757d89c1fdfc2ecc778a84f9a072a46d72b08d04
Andreas Wålm <andreas@walm.net>
2014-09-09 16:45:27 +0200

    set utf8 as tika default encoding

--- tika-meta.sh
+++ tika-meta.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser app java -jar /opt/tika/tika-app.jar -j -s 4500
+exec /sbin/setuser app java -jar /opt/tika/tika-app.jar -j -s 4500 --encoding=UTF-8
--- tika.sh
+++ tika.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser app java -jar /opt/tika/tika-app.jar -T -s 4501
+exec /sbin/setuser app java -jar /opt/tika/tika-app.jar -T -s 4501 --encoding=UTF-8
